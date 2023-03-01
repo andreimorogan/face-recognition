d4b20b86fa1f9bfb95db301577e66893a7a8d899
andreymorogan <andreymorogan@gmail.com>
2023-03-01 16:30:02 +0200

    Started work on clarifai api integration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
+import React, { Component } from 'react';
 import './App.css';
-import Navigation from './components/Navigation/Navigation'
-import Logo from './components/Logo/Logo'
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
-import Rank from './components/Rank/Rank.js'
+import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank.js';
 import Particle from './components/Particle/Particle';
-
 import 'tachyons'
+import Clarifai, { COLOR_MODEL } from 'clarifai';
 
 
-function App() {
+
+const app = new Clarifai.App({
+ apiKey: 'e334381dafd341adbf3d086b67332b17'
+});
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+    }
+  }
+
+
+onInputChange = (event) => {
+  this.setState({input: event.target.value});
+}
+
+onButtonSubmit = () => {
+  this.setState({imageUrl: this.state.input}); 
+  app.models
+    .predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+    .then(
+    function(response) {
+      console.log(response.outputs[0].data.regions[9].region_info.bounding_box);
+    },
+    function(err) {
+      // there was an error
+    }
+  )
+}
+
+render(){
   return (
     <div className="App">
+      <Particle />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
-      <Particle />
-      {/* <FaceRecognition /> */}
+      <ImageLinkForm onInputChange={this.onInputChange} 
+      onButtonSubmit={this.onButtonSubmit}/>
+      <FaceRecognition imageUrl={this.state.imageUrl} />
     </div>
   );
+}
 }
 
 export default App;
